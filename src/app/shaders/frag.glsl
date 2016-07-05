f9a48ca8b6f0dd24ca57ddf57f9724a98be82b16
Vladislav Nikonov <pacmancoder@gmail.com>
2016-07-05 11:14:48 +0300

    [Fix] suffix f removed from constant in GLSL shader

--- src/app/shaders/frag.glsl
+++ src/app/shaders/frag.glsl
@@ -6,5 +6,5 @@ uniform sampler2D tex_border;
 
 void main() {
     vec4 scr = texture2D(tex_screen, v_tex_coord);
-    gl_FragColor =  scr + texture2D(tex_border, v_tex_coord) * (1.0f - scr.a);
+    gl_FragColor =  scr + texture2D(tex_border, v_tex_coord) * (1.0 - scr.a);
 }
