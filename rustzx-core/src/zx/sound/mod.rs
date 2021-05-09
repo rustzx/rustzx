e4b4711289a1d04212f376903479ee9bd11b6e08
Vladyslav Nikonov <mail@pacmancoder.xyz>
2021-05-09 17:15:12 +0300

    Implemented configurable sound sample rate
    Closes #62

--- rustzx-core/src/zx/sound/mod.rs
+++ rustzx-core/src/zx/sound/mod.rs
@@ -5,23 +5,3 @@ pub mod sample;
 
 pub(crate) mod beeper;
 pub(crate) mod mixer;
-
-use crate::zx::constants::FPS;
-
-// TODO(#62): Make sample rate configurable
-pub const SAMPLE_RATE: usize = 44100;
-/// samples per frame
-pub const SAMPLES: usize = SAMPLE_RATE / FPS;
-// TODO: rename to samples per frame
-pub const CHANNELS: usize = 2;
-
-/// Returns, which must be already processed at this time
-pub(crate) fn samples_from_time(time: f64) -> usize {
-    if time >= 1.0 {
-        SAMPLES
-    } else if time <= 0.0 {
-        0
-    } else {
-        (SAMPLES as f64 * time) as usize
-    }
-}
