8125318a367727d178be494aeca6cb78529dd372
Vladyslav Nikonov <mail@pacmancoder.xyz>
2021-05-09 16:28:48 +0300

    Make feature gates for resource-heavy features
    Closes #57

--- rustzx-core/src/zx/mod.rs
+++ rustzx-core/src/zx/mod.rs
@@ -2,6 +2,7 @@
 //! One of core platform-independent modules
 pub(crate) mod controller;
 pub(crate) mod memory;
+#[cfg(feature = "embedded-roms")]
 pub(crate) mod roms;
 pub(crate) mod tape;
 
@@ -9,5 +10,6 @@ pub mod constants;
 pub mod joy;
 pub mod keys;
 pub mod machine;
+#[cfg(feature = "sound")]
 pub mod sound;
 pub mod video;
--- rustzx-core/src/zx/video/mod.rs
+++ rustzx-core/src/zx/video/mod.rs
@@ -1,5 +1,6 @@
 //! Module contains all screen-rendering platform-independent
 //! types and functions
+#[cfg(feature = "precise-border")]
 pub(crate) mod border;
 pub(crate) mod screen;
 
