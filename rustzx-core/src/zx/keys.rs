3bce64d2d508cfe46fd5d56ab94fbcb55ed1f4c1
Vladyslav Nikonov <mail@pacmancoder.xyz>
2021-05-10 07:43:01 +0300

    Eliminated ZXKey constants
    Closes #67

--- rustzx-core/src/zx/keys.rs
+++ rustzx-core/src/zx/keys.rs
@@ -1,79 +1,64 @@
 //! Module with hardware key port\masks
 
 /// Struct, which contains mast and port of key
-pub struct ZXKey {
-    pub(crate) half_port: u8,
-    pub(crate) mask: u8,
+#[rustfmt::skip]
+#[derive(Clone, Copy)]
+pub enum ZXKey {
+    // Port 0xFEFE
+    Shift, Z, X, C, V,
+    // Port 0xFDFE
+    A, S, D, F, G,
+    // Port 0xFBFE
+    Q, W, E, R, T,
+    // Port 0xF7FE
+    N1, N2, N3, N4, N5,
+    // Port 0xEFFE
+    N0, N9, N8, N7, N6,
+    // Port 0xDFFE
+    P, O, I, U, Y,
+    // Port 0xBFFE
+    Enter, L, K, J, H,
+    // Port 0x7FFE
+    Space, SymShift, M, N, B,
 }
 
 impl ZXKey {
-    pub(crate) fn row_id(&self) -> Option<usize> {
-        match self.half_port {
-            0xFE => Some(0),
-            0xFD => Some(1),
-            0xFB => Some(2),
-            0xF7 => Some(3),
-            0xEF => Some(4),
-            0xDF => Some(5),
-            0xBF => Some(6),
-            0x7F => Some(7),
-            _ => None,
+    pub(crate) fn row_id(self) -> usize {
+        match self.half_port() {
+            0xFE => 0,
+            0xFD => 1,
+            0xFB => 2,
+            0xF7 => 3,
+            0xEF => 4,
+            0xDF => 5,
+            0xBF => 6,
+            0x7F => 7,
+            _ => unreachable!(),
         }
     }
-}
 
-pub use predefined_keys::*;
+    pub(crate) fn mask(&self) -> u8 {
+        use ZXKey::*;
+        match self {
+            Shift | A | Q | N1 | N0 | P | Enter | Space => 0x01,
+            Z | S | W | N2 | N9 | O | L | SymShift => 0x02,
+            X | D | E | N3 | N8 | I | K | M => 0x04,
+            C | F | R | N4 | N7 | U | J | N => 0x08,
+            V | G | T | N5 | N6 | Y | H | B => 0x10,
+        }
+    }
 
-#[rustfmt::skip]
-mod predefined_keys {
-    use super::ZXKey;
-
-    // 0xFEFE
-    pub const ZX_KEY_SHIFT: ZXKey = ZXKey { half_port: 0xFE, mask: 0x01 };
-    pub const ZX_KEY_Z: ZXKey = ZXKey { half_port: 0xFE, mask: 0x02 };
-    pub const ZX_KEY_X: ZXKey = ZXKey { half_port: 0xFE, mask: 0x04 };
-    pub const ZX_KEY_C: ZXKey = ZXKey { half_port: 0xFE, mask: 0x08 };
-    pub const ZX_KEY_V: ZXKey = ZXKey { half_port: 0xFE, mask: 0x10 };
-    // FDFE
-    pub const ZX_KEY_A: ZXKey = ZXKey { half_port: 0xFD, mask: 0x01 };
-    pub const ZX_KEY_S: ZXKey = ZXKey { half_port: 0xFD, mask: 0x02 };
-    pub const ZX_KEY_D: ZXKey = ZXKey { half_port: 0xFD, mask: 0x04 };
-    pub const ZX_KEY_F: ZXKey = ZXKey { half_port: 0xFD, mask: 0x08 };
-    pub const ZX_KEY_G: ZXKey = ZXKey { half_port: 0xFD, mask: 0x10 };
-    // FBFE
-    pub const ZX_KEY_Q: ZXKey = ZXKey { half_port: 0xFB, mask: 0x01 };
-    pub const ZX_KEY_W: ZXKey = ZXKey { half_port: 0xFB, mask: 0x02 };
-    pub const ZX_KEY_E: ZXKey = ZXKey { half_port: 0xFB, mask: 0x04 };
-    pub const ZX_KEY_R: ZXKey = ZXKey { half_port: 0xFB, mask: 0x08 };
-    pub const ZX_KEY_T: ZXKey = ZXKey { half_port: 0xFB, mask: 0x10 };
-    // F7FE
-    pub const ZX_KEY_1: ZXKey = ZXKey { half_port: 0xF7, mask: 0x01 };
-    pub const ZX_KEY_2: ZXKey = ZXKey { half_port: 0xF7, mask: 0x02 };
-    pub const ZX_KEY_3: ZXKey = ZXKey { half_port: 0xF7, mask: 0x04 };
-    pub const ZX_KEY_4: ZXKey = ZXKey { half_port: 0xF7, mask: 0x08 };
-    pub const ZX_KEY_5: ZXKey = ZXKey { half_port: 0xF7, mask: 0x10 };
-    // EFFE
-    pub const ZX_KEY_0: ZXKey = ZXKey { half_port: 0xEF, mask: 0x01 };
-    pub const ZX_KEY_9: ZXKey = ZXKey { half_port: 0xEF, mask: 0x02 };
-    pub const ZX_KEY_8: ZXKey = ZXKey { half_port: 0xEF, mask: 0x04 };
-    pub const ZX_KEY_7: ZXKey = ZXKey { half_port: 0xEF, mask: 0x08 };
-    pub const ZX_KEY_6: ZXKey = ZXKey { half_port: 0xEF, mask: 0x10 };
-    //DFFE
-    pub const ZX_KEY_P: ZXKey = ZXKey { half_port: 0xDF, mask: 0x01 };
-    pub const ZX_KEY_O: ZXKey = ZXKey { half_port: 0xDF, mask: 0x02 };
-    pub const ZX_KEY_I: ZXKey = ZXKey { half_port: 0xDF, mask: 0x04 };
-    pub const ZX_KEY_U: ZXKey = ZXKey { half_port: 0xDF, mask: 0x08 };
-    pub const ZX_KEY_Y: ZXKey = ZXKey { half_port: 0xDF, mask: 0x10 };
-    // BFFE
-    pub const ZX_KEY_ENTER: ZXKey = ZXKey { half_port: 0xBF, mask: 0x01 };
-    pub const ZX_KEY_L: ZXKey = ZXKey { half_port: 0xBF, mask: 0x02 };
-    pub const ZX_KEY_K: ZXKey = ZXKey { half_port: 0xBF, mask: 0x04 };
-    pub const ZX_KEY_J: ZXKey = ZXKey { half_port: 0xBF, mask: 0x08 };
-    pub const ZX_KEY_H: ZXKey = ZXKey { half_port: 0xBF, mask: 0x10 };
-    // 7FFE
-    pub const ZX_KEY_SPACE: ZXKey = ZXKey { half_port: 0x7F, mask: 0x01 };
-    pub const ZX_KEY_SYM_SHIFT: ZXKey = ZXKey { half_port: 0x7F, mask: 0x02 };
-    pub const ZX_KEY_M: ZXKey = ZXKey { half_port: 0x7F, mask: 0x04 };
-    pub const ZX_KEY_N: ZXKey = ZXKey { half_port: 0x7F, mask: 0x08 };
-    pub const ZX_KEY_B: ZXKey = ZXKey { half_port: 0x7F, mask: 0x10 };
+    fn half_port(self) -> u8 {
+        use ZXKey::*;
+        match self {
+            Shift | Z | X | C | V => 0xFE,
+            A | S | D | F | G => 0xFD,
+            Q | W | E | R | T => 0xFB,
+            N1 | N2 | N3 | N4 | N5 => 0xF7,
+            N0 | N9 | N8 | N7 | N6 => 0xEF,
+            P | O | I | U | Y => 0xDF,
+            Enter | L | K | J | H => 0xBF,
+            Space | SymShift | M | N | B => 0x7F,
+        }
+    }
 }
